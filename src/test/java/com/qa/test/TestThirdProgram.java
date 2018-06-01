2f86f86ee8ce4e7eaef69bd9c5e48eb976ef942d
unknown <you@example.com>
2018-06-01 10:41:01 +0530

    pushing the latest code

---
+++ src/test/java/com/qa/test/TestThirdProgram.java
@@ -0,0 +1,51 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class TestThirdProgram {
+
+	@Test
+	public void sum()
+	{
+	  int a=10;
+	  int b=20;
+	  int summ;
+	  summ=a+b;
+	  
+	  System.out.println("Addition result:"+summ);
+	}
+
+	@Test
+	public void sub()
+	{
+	  int a=10;
+	  int b=20;
+	  int subb;
+	  subb=b-a;
+	  
+	  System.out.println("Substraction result:" +subb);
+	}
+	@Test
+	public void mul()
+	{
+	  int a=10;
+	  int b=20;
+	  int mull;
+	  mull=a*b;
+	  
+	  System.out.println("Multipication result:" +mull);
+	}
+	@Test
+	public void div()
+	{
+	  int a=10;
+	  int b=20;
+	  int divv;
+	  divv=b/a;
+	  
+	  System.out.println("Division Result:" +divv);
+	}
+
+
+	}
+
